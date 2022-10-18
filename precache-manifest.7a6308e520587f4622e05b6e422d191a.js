34d8a6d1402aacbe5d70969073bcc9508a03a48c
yamolatix <yamilaortiztoscano@gmail.com>
2022-10-18 16:30:08 -0300

    Updates

--- precache-manifest.7a6308e520587f4622e05b6e422d191a.js
+++ precache-manifest.7a6308e520587f4622e05b6e422d191a.js
@@ -28,12 +28,12 @@ self.__precacheManifest = [
     "url": "/themoviecode-front/static/media/slick.b7c9e1e4.woff"
   },
   {
-    "revision": "d41f55a78e6f49a5512878df1737e58a",
-    "url": "/themoviecode-front/static/media/slick.d41f55a7.ttf"
-  },
-  {
     "revision": "f97e3bbf73254b0112091d0192f17aec",
     "url": "/themoviecode-front/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/themoviecode-front/static/media/slick.d41f55a7.ttf"
   },
   {
     "revision": "277e1c5018a039a20c183b5b000d96e4",
