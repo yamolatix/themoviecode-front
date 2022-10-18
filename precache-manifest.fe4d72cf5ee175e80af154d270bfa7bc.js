407d6d6d8ce7e8e7ab904a11f5211c3d0e329d91
yamolatix <yamilaortiztoscano@gmail.com>
2022-10-18 16:23:32 -0300

    Updates

--- precache-manifest.fe4d72cf5ee175e80af154d270bfa7bc.js
+++ precache-manifest.fe4d72cf5ee175e80af154d270bfa7bc.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "84c6fc9d4b7e04870477",
+    "revision": "22e3dd14580a16248ba7",
     "url": "/themoviecode-front/static/css/main.a038aa4b.chunk.css"
   },
   {
-    "revision": "84c6fc9d4b7e04870477",
-    "url": "/themoviecode-front/static/js/main.075f178f.chunk.js"
+    "revision": "22e3dd14580a16248ba7",
+    "url": "/themoviecode-front/static/js/main.0ba94698.chunk.js"
   },
   {
     "revision": "359add09995954e8a45a",
@@ -28,15 +28,15 @@ self.__precacheManifest = [
     "url": "/themoviecode-front/static/media/slick.b7c9e1e4.woff"
   },
   {
-    "revision": "f97e3bbf73254b0112091d0192f17aec",
-    "url": "/themoviecode-front/static/media/slick.f97e3bbf.svg"
-  },
-  {
     "revision": "d41f55a78e6f49a5512878df1737e58a",
     "url": "/themoviecode-front/static/media/slick.d41f55a7.ttf"
   },
   {
-    "revision": "ceb50a9868c8aa41a3ea24a69d5ad240",
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/themoviecode-front/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "277e1c5018a039a20c183b5b000d96e4",
     "url": "/themoviecode-front/index.html"
   }
 ];
